b652625cc315f2dcc9068a92bffbfd318ba3682b
Nebojsa Tomic <nebojsatmc@gmail.com>
2021-04-15 10:05:57 +0200

    update script that enables easy start with laravel

--- src/laravel.sh
+++ src/laravel.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 ./composer.phar update
-mv ./.env.example ./.env
+cp ./.env.example ./.env
+chmod 777 -R storage # 777 should be removed, temporary do it like this
+touch ./CSS/userCSS/default_proba.css
+touch ./JS/userJS/default_proba.js
+chmod 777 -R CSS/userCSS
+chmod 777 -R JS/userJS
