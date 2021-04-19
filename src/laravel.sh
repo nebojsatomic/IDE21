95a5c201fae4836ea8c21b9f3355e4190591e28b
Nebojsa Tomic <nebojsatmc@gmail.com>
2021-04-19 14:22:30 +0200

    update setup script

--- src/laravel.sh
+++ src/laravel.sh
@@ -3,8 +3,8 @@ apt update
 apt install -y zip
 ./composer.phar update
 cp ./.env.example ./.env
-chmod 777 -R storage # 777 should be removed, temporary do it like this
+
 touch ./CSS/userCSS/default_proba.css
 touch ./JS/userJS/default_proba.js
-chmod 777 -R CSS/userCSS
-chmod 777 -R JS/userJS
+
+chown -R www-data ./
