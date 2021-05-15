f1fceb02a6db3e49cd03b9b2dc62b91f9c970c04
Nebojsa Tomic <nebojsatmc@gmail.com>
2021-05-15 10:14:53 +0200

    update laravel setup script

--- src/laravel.sh
+++ src/laravel.sh
@@ -5,3 +5,8 @@ apt install -y zip
 cp ./.env.example ./.env
 
 chown -R www-data .
+
+GREEN='\033[0;32m'
+RED='\033[1;31m'
+YELLOW='\033[1;33m'
+echo -e "${GREEN}Done! Do ${RED}Ctrl+C ${GREEN}to exit and then ${YELLOW}docker-compose up"
