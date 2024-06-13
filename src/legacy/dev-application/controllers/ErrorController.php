66a5ff0bf46c5ba95081e95a2b75119e4382e22e
Nebojsa Tomic <nebojsatmc@gmail.com>
2024-06-13 12:13:48 +0200

    saving a new page with the default template_id now, updated 404 page

--- src/legacy/dev-application/controllers/ErrorController.php
+++ src/legacy/dev-application/controllers/ErrorController.php
@@ -18,7 +18,7 @@
  
 require_once 'NeTFramework/NetActionController.php';
 
-class ErrorController extends Zend_Controller_Action
+class ErrorController extends NetActionController
 {
     public static $host;
     protected static $lang;
@@ -27,52 +27,25 @@ class ErrorController extends Zend_Controller_Action
     {
         define("NET_PATH_SITE", $this->_nps);
         define("NET_PATH", $this->_np);
-              
+
         $this->view->host = $this->_host;
         self::$host = NetActionController::$host;
         self::$lang = $this->_sesija->lang;
         
         $request = $this->getRequest();
         if ($request->isXmlHttpRequest()) {
-           
+
         }
-         $this->_helper->layout()->disableLayout();
+        $this->_helper->layout()->disableLayout();
     }
-
-    public function init2()
-    {
-/*
-        parent::init();
-
-        $bootstrap = $this->getInvokeArg('bootstrap');
-
-        $environment = $bootstrap->getEnvironment();
-        $error = $this->_getParam('error_handler');
-        $mailer = new Zend_Mail();
-        $session = new Zend_Session_Namespace();
-        $database = $bootstrap->getResource('Database');
-        $profiler = $database->getProfiler();
-
-        $this->_notifier = new Application_Service_Notifier_Error(
-            $environment,
-            $error,
-            $mailer,
-            $session,
-            $profiler,
-            $_SERVER
-        );
-
-        $this->_error = $error;
-        $this->_environment = $environment;
-*/
-   }
 
     public function indexAction()
     {
-        $this->_helper->layout->setLayoutPath($this->_np . 'layouts/scripts/errorPages/')->setLayout('404');    
+        $this->_helper->layout->setLayoutPath($this->_np . 'layouts/scripts/errorPages/')->setLayout('404');
     }
-        public function errorAction()
-        {
-            $this->_helper->layout->setLayoutPath($this->_np . 'layouts/scripts/errorPages/')->setLayout('404');        
-        }
+
+    public function errorAction()
+    {
+        $this->_helper->layout->setLayoutPath($this->_np . 'layouts/scripts/errorPages/')->setLayout('404');
+    }
 }
