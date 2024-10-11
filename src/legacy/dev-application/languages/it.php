8976ff9c5218b39a0cc349b2678a6004488980bd
Matteo Gheza <matteo.gheza07@gmail.com>
2024-10-11 16:46:30 +0200

    Add other italian translation files

---
+++ src/legacy/dev-application/languages/it.php
@@ -0,0 +1,79 @@
+<?php
+
+return array(
+"Prva" => "Prima categoria",
+
+"First Name" => "Nome:",
+"Last Name" => "Cognome:",
+"First name" => "Nome:",
+"Last name" => "Cognome:",
+"Company" => "Azienda:",
+"eMail address" => "Indirizzo email:",
+"Email address" => "Indirizzo email:",
+"Phone number" => "Numero di telefono:",
+"Message" => "Messaggio",
+"Submit" => "Invia",
+"Lokacija" => "Posizione",
+
+"Message sent :<br />" => "Messaggio inviato:<br />",
+"<br />Mail has been sent! Thank you." => "La mail è stata inviata. Grazie!",
+"Value is required and can't be empty" => "Il valore è obbligatorio e non può essere vuoto",
+
+"Cenovnik" => "Listino prezzi",
+
+"Services" => "Servizi",
+
+"Prices" => "Prezzi",
+"Search" => "Cerca",
+
+"Welcome" => "Benvenuto",
+"Welcome Guest" => "Benvenuto Ospite",
+
+"You need to Login!" => "Devi effettuare il login!",
+"Account Information" => "Informazioni sull'account",
+"Username" => "Nome utente",
+"Password" => "Password",
+"Repeat Password" => "Ripeti Password",
+"Full Name" => "Nome completo",
+"Update" => "Aggiorna",
+"Register" => "Registrati",
+"ChangePassword" => "Cambia password",
+"Change"  => "Cambia",
+"New Password" => "Nuova password",
+"New Password retype" => "Riscrivi la nuova password",
+"The username you selected already exists." => "Il nome utente selezionato esiste già.",
+"The email you entered already exists." => "L'email inserita esiste già.",
+"The supplied passwords don't match." => "Le password inserite non coincidono!",
+"Your account has been created, you have been provided with activation link, check your mail!" => "Il tuo account è stato creato. È stato inviato un link di attivazione, controlla la tua mail!",
+"Your Account has been activated!You can now login!" => "Il tuo account è stato attivato! Ora puoi effettuare il login!",
+"Your activation link is invalid!" => "Il tuo link di attivazione non è valido!",
+
+"There is no user with these credentials!" => "Non esiste un utente con queste credenziali!",
+"Your Account, " => "Il tuo Account, ",
+"You are not logged in!Please Login to see your account details!" => "Non sei connesso! Effettua il login per vedere i dettagli del tuo account!",
+"Password reminder is sent to the provided email!" => "Promemoria della password inviato all'email fornita!",
+
+"Please verify you're a human" => "Per favore verifica di essere umano",
+"Captcha value is wrong" => "Il valore del Captcha è errato",
+"IDE21 current projects" => "Progetti attuali di IDE21",
+
+// ADMIN part - NeT.Creator
+"Page/Object" => "Pagina/Oggetto",
+"Menus" => "Menu",
+"Categories" => "Categorie",
+"Languages" => "Lingue",
+"Modules" => "Moduli",
+"Images" => "Immagini",
+"Settings" => "Impostazioni",
+"Object Editors" => "Editor dell'oggetto",
+"View Page" => "Visualizza pagina",
+"Style for this object:" => "Stile per questo oggetto",
+"Update object" => "Aggiorna oggetto",
+"Page"  => "Pagina",
+"Template" => "Modello",
+"Empty Cache" => "Svuota cache",
+"Backup Site" => "Backup del sito",
+"Back" => "Indietro",
+"Next" => "Avanti",
+);
+
