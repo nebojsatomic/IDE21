fa052fbdb0a1757d056e5edaa87b6fa1a58096b0
acacio90 <pedrorodriguesnh@gmail.com>
2024-10-24 16:45:51 -0300

    Adding Portuguese translation

---
+++ src/legacy/dev-application/languages/pt.php
@@ -0,0 +1,36 @@
+<?php
+return array(
+"Prva" => "Primeira categoria",
+"First Name" => "Primeiro nome",
+"Last Name" => "Sobrenome" ,
+"First name" => "Primeiro nome",
+"Last name" => "Sobrenome",
+"Company" => "Empresa",
+"eMail address" => "Endereço de e-mail",
+"Email address" => "Endereço de e-mail",
+"Phone number" => "Número de telefone",
+"Message" => "Mensagem",
+"Submit" => "Enviar" ,
+
+"Message sent :<br />" => "Mensagem enviada :<br />",
+"<br />Mail has been sent! Thank you." => "<br />O e-mail foi enviado! Obrigado.",
+"Value is required and can't be empty" => "O valor é obrigatório e não pode estar vazio",
+
+
+"Lokacija" => "Localização",
+"Cenovnik" => "Preços",
+"Rezervacije" => "Reservas",
+"Sve o nama" => "Sobre nós",
+
+"Welcome" => "Bem-vindo",
+"Welcome Guest" => "Bem-vindo, convidado",
+
+"You need to Login!" => "Você precisa fazer login!",
+"Account Information" => "Informações da conta",
+"Password" => "Senha",
+"Repeat Password" => "Repita a senha",
+
+"Please verify you're a human" => "Por favor, verifique se você é humano",
+"Captcha value is wrong" => "O valor do Captcha está incorreto",
+
+);
